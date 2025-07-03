ac33e3d2f835289ca508f25266a4746861418b48
Buddha_gog <karalisalexander@yandex.ru>
2025-07-03 16:41:14 +0300

    Com20

--- BurstingMachine/BurstingMachine/Pages/Chart.xaml.cs
+++ BurstingMachine/BurstingMachine/Pages/Chart.xaml.cs
@@ -1,57 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ScottPlot;
-using ScottPlot.Plottables;
-using ScottPlot.WPF;
 
 namespace BurstingMachine.Pages
 {
     public partial class Chart : Page
     {
-
+        public static int PointNum; 
         public Chart()
         {
             InitializeComponent();
         }
-
-        private void btnStop_Click(object sender, RoutedEventArgs e)
+        private async void LoadNewtons()
         {
-            double[] dataX = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            double[] dataY = { 1, 4, 6, 8, 10, 12, 14, 16, 10 };
+            for (double i = 0; i < 10; i += 0.10) 
+            {
+                TBNewtons.Text = i.ToString($"F{PointNum}");
+                await Task.Delay(500); // 1 секунда паузы
+            }
+        }
+        private async void LoadSpeed()
+        {
+            
+            for (double i = 0; i < 10; i += 0.10)
+            {
+                TBSpeed.Text = i.ToString($"F{PointNum}");
+                await Task.Delay(500); // 1 секунда паузы
+            }
+        }
+        private async void LoadPlot()
+        {
+            List<double> dataX = new();
+            List<double> dataY = new();
+
+            double[] FulldataX = { 1, 2, 3, 4, 5, 6, 7, 8, 9}; //Чтобы работало колличество точек должно быть равным 
+            double[] FulldataY = { 1, 4, 6, 8, 10, 12, 14, 16, 10}; //Чтобы работало колличество точек должно быть равным 
+
             var myPlot = WpfPlot1.Plot.Add.Scatter(dataX, dataY);
             myPlot.LineColor = ScottPlot.Colors.Red;
             myPlot.MarkerColor = ScottPlot.Colors.Red;
-            WpfPlot1.Refresh();
-        }
 
-        private void btnNormalize_Click(object sender, RoutedEventArgs e)
+            WpfPlot1.Refresh();
+
+            for (int i = 0; i < FulldataX.Length; i++)
+            {
+                dataX.Add(FulldataX[i]);
+                dataY.Add(FulldataY[i]);
+
+                AScale();
+                await Task.Delay(500);
+            }
+        }
+        private void AScale()
         {
             WpfPlot1.Plot.Axes.AutoScale();
             WpfPlot1.Refresh();
         }
-
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPlot();
+            LoadNewtons();
+            LoadSpeed();
+        }
+        private void btnNormalize_Click(object sender, RoutedEventArgs e)
+        {
+            AScale();
+        }
+        /*1.0 = без изменения,
+        < 1 = приближение,
+        > 1 = отдаление.*/
         private void btnIncrease_Click(object sender, RoutedEventArgs e)
         {
-           
+            WpfPlot1.Plot.Axes.Zoom(1.1); //Приближает на 10%
+            WpfPlot1.Refresh();
         }
-
         private void btnReduce_Click(object sender, RoutedEventArgs e)
         {
-
+            WpfPlot1.Plot.Axes.Zoom(0.9); //Отдаляет на 10%
+            WpfPlot1.Refresh();
         }
     }
 }
